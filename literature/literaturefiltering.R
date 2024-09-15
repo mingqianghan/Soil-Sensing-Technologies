d198865a8d536b838bf23de6c8bca5b929fe9c69
Mingqiang Han <49960231+mingqianghan@users.noreply.github.com>
2024-09-15 00:38:05 -0500

    Update literature

--- literature/literaturefiltering.R
+++ literature/literaturefiltering.R
@@ -9,11 +9,26 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 current_dir <- getwd()
 
 # Create a file path for the "savedrecs.bib" file
-file_location <- file.path(current_dir, "savedrecs.bib")
+file1 <- file.path(current_dir, "webofscience_1.bib")
+file2 <- file.path(current_dir, "webofscience_2.bib")
+
+
+# Combine them into a single data frame
+combined_bib <- rbind(file1, file2)
 
 # Import the bibliographic information from the .bib file into a data.frame
-data <- read_bibliography(file_location)
+data <- read_bibliography(combined_bib)
+
+# Print the total number of literature entries in the combined bibliography
+cat("Total number of literature entries:", nrow(data), "\n")
+
+x_check <- find_duplicates(data)
+x_unique <- extract_unique_references(data, matches = x_check)
+
+# Print the total number of literature entries in the combined bibliography
+cat("Total number of unique literature entries:", nrow(x_unique), "\n")
+
 
 # Launch a graphical interface to screen the topics in the imported data
-screen_topics(data)
+# screen_topics(data)
 
